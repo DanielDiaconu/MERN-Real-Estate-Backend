1baad32bbee33b88ef0222ec3b2f625c354c1b0e
Daniel Diaconu <daniel.diaconu234@gmail.com>
2021-10-10 17:40:32 +0300

    established socket connection to client

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 require("dotenv/config");
 const express = require("express");
 const mongoose = require("mongoose");
+const http = require("http");
 const app = express();
 const PORT = 8080;
 const cors = require("cors");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const categoriesRoute = require("./routes/categories");
@@ -35,7 +43,20 @@ app.use("/", questionsRoute);
 app.use("/", repliesRoute);
 app.use("/reviews", reviewsRoute);
 
-app.listen(PORT, console.log(`We are live and listening on port ${PORT}`));
+io.on("connection", (socket) => {
+  console.log(`user connected: ${socket.id}`);
+
+  socket.on("join-room", (data) => {
+    socket.join(data);
+    console.log(`user with id: ${socket.id} joined room ${data}`);
+  });
+
+  socket.on("disconnect", () => [
+    console.log(`User discoonected ${socket.id}`),
+  ]);
+});
+
+server.listen(PORT, console.log(`We are live and listening on port ${PORT}`));
 mongoose.connect(
   process.env.DB_CONNECTION,
   console.log("Connected to Database !")
