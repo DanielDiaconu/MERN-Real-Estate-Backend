7377ee5e3e89d295f43ced3b0d90c713d81c9509
Daniel Diaconu <daniel.diaconu234@gmail.com>
2021-10-27 14:54:49 +0300

    added cors rules

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://mern-online-properties.netlify.app/",
+      "https://mern-online-properties.netlify.app",
     ],
   },
 });
